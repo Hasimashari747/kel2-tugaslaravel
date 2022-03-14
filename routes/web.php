c8309a594e24fa75c7df26718fbf55cd0f8f5994
Sella Putri Sari <sellaputrisari9@gmail.com>
2022-03-14 20:28:49 +0700

    Penambahan navbar, view home dan buku

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/buku', function () {
+    return view('buku', [
+        "title" => "Buku"
+    ]);
 });
